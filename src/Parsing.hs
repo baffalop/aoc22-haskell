67f866ed859036a7cfe5cd2c5128e2906eec9ab4
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-05 00:36:45 +0000

    No need for endOfInput in linesOf

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -5,10 +5,9 @@ module Parsing
 
 import Data.Attoparsec.Text (Parser)
 import qualified Data.Attoparsec.Text as P
-import Control.Applicative ((<|>))
 
 linesOf :: Parser a -> Parser [a]
-linesOf = (`P.sepBy` (P.endOfLine <|> P.endOfInput))
+linesOf = (`P.sepBy` P.endOfLine)
 
 pairBy :: Char -> Parser a -> Parser (a, a)
 pairBy sep p = (,) <$> p <* P.char sep <*> p

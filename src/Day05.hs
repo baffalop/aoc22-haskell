9d7ec24c50e496df94ed252487c01ea2fe09cff5
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-05 13:39:16 +0000

    Solve day 5 part 1

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,24 +1,27 @@
 module Day05 (parse, solve1, solve2) where
 
 import Data.Text (Text)
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 import qualified Data.Attoparsec.Text as P
 import Data.Attoparsec.Text (Parser)
 import Parsing (linesOf)
-import Data.Maybe (catMaybes)
-import Data.List (transpose)
+import Data.Maybe (catMaybes, fromMaybe)
+import Data.List (transpose, uncons)
 import Control.Applicative ((<|>))
+import Data.Foldable (foldl')
 
 data Input = Input
-  { _stacks :: Stacks
-  , _instructions :: [Instruction]
+  { stacks :: Stacks
+  , instructions :: [Instruction]
   } deriving (Show)
 
-type Stacks = [String]
+type Stacks = Seq String
 
 data Instruction = Instruction
-  { _take :: Int
-  , _from :: Int
-  , _to :: Int
+  { n :: Int
+  , from :: Int
+  , to :: Int
   } deriving (Show)
 
 parse :: Text -> Either String Input
@@ -28,12 +31,11 @@ parse = P.parseOnly $ Input
   <*> linesOf instruction
   where
     stacks :: Parser Stacks
-    stacks = fmap catMaybes . transpose <$> linesOf (stack `P.sepBy` P.char ' ')
+    stacks = Seq.fromList . fmap catMaybes . transpose <$> linesOf (stack `P.sepBy` P.char ' ')
 
     stack :: Parser (Maybe Char)
-    stack =
-      Just <$ P.char '[' <*> P.letter <* P.char ']'
-        <|> Nothing <$ P.string "   "
+    stack = Just <$ P.char '[' <*> P.letter <* P.char ']'
+      <|> Nothing <$ P.string "   "
 
     indices :: Parser [Int]
     indices = P.decimal `P.sepBy` P.skipSpace
@@ -45,5 +47,27 @@ parse = P.parseOnly $ Input
       <* P.string " to " <*> P.decimal
 
 
-solve1 = undefined
+solve1 :: Input -> String
+solve1 Input{ stacks, instructions } =
+  catMaybes $ toList $ headMay <$> foldl' (flip run) stacks instructions
+
+run :: Instruction -> Stacks -> Stacks
+run Instruction{ n, from, to } = nTimes n apply
+  where
+    apply stacks = fromMaybe stacks $ do
+      (top, rest) <- uncons =<< Seq.lookup (from - 1) stacks
+      pure $ Seq.adjust' (top :) (to - 1) $ Seq.update (from - 1) rest stacks
+
 solve2 = undefined
+
+nTimes :: Int -> (a -> a) -> a -> a
+nTimes n f x
+  | n <= 0 = x
+  | otherwise = nTimes (n - 1) f $ f x
+
+toList :: Seq a -> [a]
+toList = foldr (:) []
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x : _) = Just x
--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,13 @@ import System.Environment (getArgs)
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 import Control.Arrow ((&&&))
+import Data.Tuple.Extra (both)
 
 import qualified Day01
 import qualified Day02
 import qualified Day03
 import qualified Day04
+import qualified Day05
 
 type Day = Int
 
@@ -25,14 +27,15 @@ main = do
         Left err ->
           putStrLn $ "Parse error: " <> err
         Right (sol1, sol2) -> do
-          putStrLn $ "Part 1: " <> show sol1
-          putStrLn $ "Part 2: " <> show sol2
+          putStrLn $ "Part 1: " <> sol1
+          putStrLn $ "Part 2: " <> sol2
 
-getSolution :: Day -> Text -> Either String (Int, Int)
-getSolution 1 = Right . (Day01.solve1 &&& Day01.solve2) . Day01.parse
-getSolution 2 = fmap (Day02.solve1 &&& Day02.solve2) . Day02.parse
-getSolution 3 = Right . (Day03.solve1 &&& Day03.solve2) . Day03.parse
-getSolution 4 = fmap (Day04.solve1 &&& Day04.solve2) . Day04.parse
+getSolution :: Day -> Text -> Either String (String, String)
+getSolution 1 = Right . both show . (Day01.solve1 &&& Day01.solve2) . Day01.parse
+getSolution 2 = fmap (both show. (Day02.solve1 &&& Day02.solve2)) . Day02.parse
+getSolution 3 = Right . both show . (Day03.solve1 &&& Day03.solve2) . Day03.parse
+getSolution 4 = fmap (both show . (Day04.solve1 &&& Day04.solve2)) . Day04.parse
+getSolution 5 = fmap (Day05.solve1 &&& Day05.solve2) . Day05.parse
 getSolution day = error $ "No solution for day " <> show day <> " yet"
 
 pad0 :: String -> String

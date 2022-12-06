c1d568ffde7c2f73609d8ddc36a3a084cfa0036a
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-06 13:57:07 +0000

    Let the answers be Maybe

--- src/Day06.hs
+++ src/Day06.hs
@@ -2,16 +2,15 @@ module Day06 (parse, solve1, solve2) where
 
 import Data.Text (Text, unpack)
 import Data.List (tails, findIndex, nub)
-import Data.Maybe (fromMaybe)
 
 parse :: Text -> String
 parse = unpack
 
-solve1 :: String -> Int
-solve1 = fromMaybe 0  . findPacket 4
+solve1 :: String -> Maybe Int
+solve1 = findPacket 4
 
-solve2 :: String -> Int
-solve2 = fromMaybe 0 . findPacket 14
+solve2 :: String -> Maybe Int
+solve2 = findPacket 14
 
 findPacket :: Int -> String -> Maybe Int
 findPacket window = fmap (+ window) . findIndex (allDistinct . take window) . tails

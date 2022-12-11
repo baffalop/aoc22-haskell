75cb2d7ad8a81b60f692a6e51fe955761947f4bf
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-11 13:28:10 +0000

    Add nTimes util

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,7 +1,11 @@
 module Utils
   ((<.>)
+  , nTimes
   ) where
 
 infixl 4 <.>
 (<.>) :: Functor f => (b -> c) -> (a -> f b) -> a -> f c
 f <.> g = fmap f . g
+
+nTimes :: Int -> (a -> a) -> a -> a
+nTimes n f = foldr (.) id $ replicate n f

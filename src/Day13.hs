c13afd73f79dbf5b4468580c3d1cc1fdf8a3810f
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-13 22:01:25 +0000

    Solve day 13

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,15 +1,50 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Day13 (parse, solve1, solve2) where
 
-import Data.Text (Text, unpack)
+import Data.Text (Text)
 import qualified Data.Attoparsec.Text as P
+import Parsing (pairBy)
+import Data.List (findIndices, sort)
+import Utils ((<.>))
 
-type Input = _
+type Input = [(Nested Int, Nested Int)]
+
+data Nested a = One a | Some [Nested a] deriving (Show, Eq)
+
+instance Ord a => Ord (Nested a) where
+  compare :: Nested a -> Nested a -> Ordering
+  compare = curry $ \case
+    (One a, One b) -> compare a b
+    (One a, bs) -> compare (Some [One a]) bs
+    (as, One b) -> compare as (Some [One b])
+    (Some [], Some []) -> EQ
+    (Some [], _) -> LT
+    (_, Some []) -> GT
+    (Some (a:as), Some (b:bs)) ->
+      if a == b then compare (Some as) (Some bs) else compare a b
 
 parse :: Text -> Either String Input
-parse = undefined
+parse = P.parseOnly $
+  pairBy '\n' nested `P.sepBy` (P.endOfLine <* P.endOfLine)
+  where
+    nested :: P.Parser (Nested Int)
+    nested = P.choice
+      [ One <$> P.decimal :: P.Parser (Nested Int)
+      , Some <$ P.char '[' <*> nested `P.sepBy` P.char ',' <* P.char ']'
+      ]
 
 solve1 :: Input -> Int
-solve1 = undefined
+solve1 = sum . indicesNat (uncurry (<))
 
 solve2 :: Input -> Int
-solve2 = undefined
+solve2 = product . indicesNat (`elem` dividers) . sort . (dividers <>) . foldMap unpair
+  where
+    dividers :: [Nested Int]
+    dividers = [Some [Some [One 2]], Some [Some [One 6]]]
+
+indicesNat :: (a -> Bool) -> [a] -> [Int]
+indicesNat f = (+ 1) <.> findIndices f
+
+unpair :: (a, a) -> [a]
+unpair (a, b) = [a, b]

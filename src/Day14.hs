76379d1cb6b1ab237068de58953e05057400b962
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-14 22:45:56 +0000

    Solve day 14 part 1

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,20 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Day14  where
+module Day14 (parse, solve1, solve2) where
 
 import Data.Text (Text)
 import qualified Data.Attoparsec.Text as P
 import Parsing (pairBy)
 import Data.IntMap (IntMap, (!?))
 import qualified Data.IntMap as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as Set
 import Utils (partitionWith, pairs, (<.>))
 import Data.Tuple.Extra (both)
 import Data.Maybe (fromMaybe)
 import Lens.Micro.Platform (makeLensesFor, (%~))
 import Data.List (find)
-import Debug.Trace (traceShowM, traceShowId)
 
 type Path = [Coord]
 type Line = (Coord, Coord)
@@ -25,7 +24,7 @@ data Cave = Cave
   , cols :: Cardinal
   } deriving (Show)
 
-type Cardinal = IntMap (Set Int)
+type Cardinal = IntMap IntSet
 
 makeLensesFor [("rows", "_rows"), ("cols", "_cols")] ''Cave
 
@@ -48,12 +47,12 @@ mapCave :: [Path] -> Cave
 mapCave =
   uncurry Cave . both cardinal . partitionWith orientation . foldMap pairs
   where
-    orientation :: Line -> Either (Int, Set Int) (Int, Set Int)
+    orientation :: Line -> Either (Int, IntSet) (Int, IntSet)
     orientation ((x1, y1), (x2, y2))
-      | x1 == x2 = Left (y1, Set.fromList [x1..x2])
-      | otherwise = Right (x1, Set.fromList [y1..y2])
+      | y1 == y2 = Left (y1, Set.fromList [min x1 x2..max x1 x2])
+      | otherwise = Right (x1, Set.fromList [min y1 y2..max y1 y2])
 
-    cardinal :: [(Int, Set Int)] -> Cardinal
+    cardinal :: [(Int, IntSet)] -> Cardinal
     cardinal = foldr (uncurry $ Map.insertWith Set.union) Map.empty
 
 flowsIn :: Cave -> Coord -> Maybe Coord
@@ -62,7 +61,6 @@ flowsIn cave = flow
     flow :: Coord -> Maybe Coord
     flow c = do
       bottom@(x, y) <- dropsTo c cave
-      traceShowM bottom
       case filter (not . blockedBy cave) $ (, y + 1) <$> [x - 1, x + 1] of
         next:_ -> flow next
         _ -> return bottom
@@ -72,7 +70,7 @@ dropsTo (x, y) Cave{..} =
   (x,) . subtract 1 <.> find (> y) $ Set.toList $ verticals <> horizontals
   where
     verticals = fromMaybe Set.empty (cols !? x)
-    horizontals = foldr (<>) Set.empty $ Map.filter (Set.member x) rows
+    horizontals = Map.keysSet $ Map.filter (Set.member x) rows
 
 blockedBy :: Cave -> Coord -> Bool
 blockedBy Cave{ rows, cols } (x, y) =

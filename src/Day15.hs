16e79dd601acc211748672f219693bb106ea4ff1
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-16 13:09:17 +0000

    Parse day 15

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,14 +2,41 @@ module Day15 (parse, solve1, solve2) where
 
 import Data.Text (Text)
 import qualified Data.Attoparsec.Text as P
+import Data.Attoparsec.Text ((<?>))
+import Parsing (linesOf)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Utils (manhattanDistance)
 
-type Input = [[Int]]
+type Coord = (Int, Int)
 
-parse :: Text -> Either String Input
-parse = undefined
+data Scan = Scan
+ { sensors :: Map Coord Int
+ , beacons :: Set Coord
+ } deriving (Show)
 
-solve1 :: Input -> Int
+instance Semigroup Scan where
+  scan1 <> scan2 = Scan (sensors scan1 <> sensors scan2) (beacons scan1 <> beacons scan2)
+
+instance Monoid Scan where
+  mempty = Scan mempty mempty
+
+parse :: Text -> Either String Scan
+parse = P.parseOnly $ mconcat <$> linesOf do
+  sensor <- (,) <$ P.string "Sensor at x=" <*> n <* P.string ", y=" <*> n <?> "sensor"
+  beacon <- (,) <$ P.string ": closest beacon is at x=" <*> n <* P.string ", y=" <*> n <?> "beacon"
+  return Scan
+    { sensors = Map.singleton sensor $ manhattanDistance sensor beacon
+    , beacons = Set.singleton beacon
+    }
+  where
+    n :: P.Parser Int
+    n = P.signed P.decimal
+
+solve1 :: Scan -> Int
 solve1 = undefined
 
-solve2 :: Input -> Int
+solve2 :: Scan -> Int
 solve2 = undefined

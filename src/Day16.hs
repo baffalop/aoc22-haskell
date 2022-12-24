d4aa36c10956123709f8ffdfc474c5593aa758e2
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-24 01:34:54 +0000

    Write a silly score function for day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -5,9 +5,11 @@ import qualified Data.Attoparsec.Text as P
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Parsing (linesOf, alphaWord)
-import Control.Monad (void)
+import Control.Monad (void, guard)
+import Data.Maybe (fromMaybe)
 
 type Valves = Map ValveKey Valve
+type Plan = [ValveKey]
 
 newtype ValveKey = Key Text deriving (Show, Eq, Ord)
 
@@ -29,3 +31,10 @@ solve1 = undefined
 
 solve2 :: Valves -> Int
 solve2 = undefined
+
+scoreFromTime :: Int -> Valves -> Plan -> Int
+scoreFromTime t valves = snd . flip foldl (t, 0) \(t', score) k ->
+  fromMaybe (t' - 1, score) do
+    flow <- flow <$> Map.lookup k valves
+    guard $ flow > 0
+    return (t' - 2, score + (t' - 2) * flow)

692f3a33628037b921a766b1730c814ded76059f
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-27 14:55:58 +0000

    Attempt a sub-optimise function for day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,22 +1,34 @@
-module Day16 (parse, solve1, solve2) where
+module Day16 where
 
 import Data.Text (Text)
 import qualified Data.Attoparsec.Text as P
 import Control.Applicative ((<|>))
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Parsing (linesOf, alphaWord)
-import Control.Monad (void, guard)
-import Data.Maybe (fromMaybe)
+import Control.Monad (void)
+import Data.Maybe (fromMaybe, catMaybes)
+import Safe (maximumByMay)
+import Data.Function (on)
+import Debug.Trace (traceShowId)
 
 type Valves = Map ValveKey Valve
-type Plan = [ValveKey]
 
 newtype ValveKey = Key Text deriving (Show, Eq, Ord)
+
+data Step = Open | GoTo ValveKey deriving (Show, Eq)
 
 data Valve = Valve
   { flow :: Int
   , tunnels :: [ValveKey]
+  } deriving (Show)
+
+data Plan = Plan
+  { score :: Int
+  , time :: Int
+  , opened :: Set ValveKey
   } deriving (Show)
 
 parse :: Text -> Either String Valves
@@ -33,9 +45,30 @@ solve1 = undefined
 solve2 :: Valves -> Int
 solve2 = undefined
 
-scoreFromTime :: Int -> Valves -> Plan -> Int
-scoreFromTime t valves = snd . flip foldl (t, 0) \(t', score) k ->
-  fromMaybe (t' - 1, score) do
-    flow <- flow <$> Map.lookup k valves
-    guard $ flow > 0
-    return (t' - 2, score + (t' - 2) * flow)
+optimumTo :: Valves -> ValveKey -> ValveKey -> Int -> Set ValveKey -> Maybe Plan
+optimumTo valves origin target t' opened =
+  findOptimum Plan { score = 0, time = t', .. } Set.empty origin
+  where
+    findOptimum :: Plan -> Set ValveKey -> ValveKey -> Maybe Plan
+    findOptimum plan@Plan{ time = 0 } _ _ = Just plan
+    findOptimum plan@Plan{ score = score', time = t, .. } visited cur =
+      let
+        Valve{..} = fromMaybe (error $ "invalid valve " <> show cur) $ Map.lookup cur valves
+        withOpenedValve =
+          if flow /= 0 && not (Set.member cur opened)
+            then (:) $ findOptimum Plan
+              { opened = Set.insert cur opened
+              , time = t - 1
+              , score = score' + (flow * (t - 1))
+              }
+              visited
+              cur
+            else id
+      in
+      if cur == target && Set.member cur opened
+        then traceShowId $ Just plan
+        else maximumByMay (compare `on` ((*) <$> score <*> time))
+          $ catMaybes
+          $ withOpenedValve
+          $ findOptimum plan{ time = t - 1 } (Set.insert cur visited)
+          <$> filter (not . (`Set.member` visited)) tunnels

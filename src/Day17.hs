6b55d105ec88834989999f0fbffb6b558e09e5b9
Nikita Gaidakov <ngaidakov@podfather.com>
2022-12-28 23:55:38 +0000

    Separate newtypes for Block and Rock

--- src/Day17.hs
+++ src/Day17.hs
@@ -11,7 +11,8 @@ data Gust = L | R deriving (Show)
 
 type Coord = (Int, Int)
 
-type Block = Set Coord
+newtype Block = Block (Set Coord)
+newtype Rock = Rock (Set Coord)
 
 parse :: Text -> Either String [Gust]
 parse = traverse gust . head . lines . unpack
@@ -22,29 +23,29 @@ parse = traverse gust . head . lines . unpack
     gust c = Left $ "Not a gust: " <> [c]
 
 solve1 :: [Gust] -> Int
-solve1 gusts = (+ 1) $ ceiling $ iterateBlocks 2022 blocks (cycle gusts) Set.empty
+solve1 gusts' = (+ 1) $ ceiling $ iterateBlocks 2022 blocks (cycle gusts') (Rock Set.empty)
   where
-    iterateBlocks :: Int -> [Int -> Block] -> [Gust] -> Block -> Block
-    iterateBlocks 0 _ _ fallen = fallen
+    iterateBlocks :: Int -> [Int -> Block] -> [Gust] -> Rock -> Rock
+    iterateBlocks 0 _ _ rock = rock
     iterateBlocks _ [] _ _ = error "empty blocks - shouldn't they be infinite?"
     iterateBlocks n (mkBlock:mkBlocks) gusts fallen =
-      let (newFallen, newGusts) = dropBlock fallen gusts $ mkBlock $ ceiling fallen + 3
-      in iterateBlocks (n - 1) mkBlocks newGusts newFallen
+      let (newRock, nextGusts) = dropBlock fallen gusts $ mkBlock $ ceiling fallen + 3
+      in iterateBlocks (n - 1) mkBlocks nextGusts newRock
 
 solve2 :: [Gust] -> Int
 solve2 = undefined
 
-dropBlock :: Block -> [Gust] -> Block -> (Block, [Gust])
-dropBlock fallen = drop
+dropBlock :: Rock -> [Gust] -> Block -> (Rock, [Gust])
+dropBlock rock = drop
   where
-    drop :: [Gust] -> Block -> (Block, [Gust])
+    drop :: [Gust] -> Block -> (Rock, [Gust])
     drop [] _ = error "empty gusts - shouldn't they be infinite?"
-    drop (gust:gusts) block = case step fallen gust block of
-      Left blocks -> (blocks, gusts)
+    drop (gust:gusts) block = case step rock gust block of
+      Left rock' -> (rock', gusts)
       Right dropped -> drop gusts dropped
 
-step :: Block -> Gust -> Block -> Either Block Block
-step fallen gust = fall . blow
+step :: Rock -> Gust -> Block -> Either Rock Block
+step rock gust = fall . blow
   where
     blow :: Block -> Block
     blow block =
@@ -53,36 +54,42 @@ step fallen gust = fall . blow
           L -> (subtract 1, hitsLeft)
           R -> ((+ 1), hitsRight)
 
-        blown = Set.map (first buffet) block
+        blown = move (first buffet) block
       in
-      if clashes blown || hitsSide blown then block else blown
+      if clashes blown rock || hitsSide blown then block else blown
 
-    fall :: Block -> Either Block Block
+    fall :: Block -> Either Rock Block
     fall block = let dropped = lower block in
-      if clashes dropped || hitsFloor dropped
-      then Left $ Set.union block fallen
+      if clashes dropped rock || hitsFloor dropped
+      then Left $ calcify block rock
       else Right dropped
 
-    clashes :: Block -> Bool
-    clashes = not . Set.disjoint fallen
+clashes :: Block -> Rock -> Bool
+clashes (Block block) (Rock rock) = not $ Set.disjoint rock block
 
-ceiling :: Block -> Int
-ceiling = fromMaybe (-1) . Set.lookupMax . Set.map snd
+ceiling :: Rock -> Int
+ceiling (Rock r) = fromMaybe (-1) $ Set.lookupMax $ Set.map snd r
+
+move :: (Coord -> Coord) -> Block -> Block
+move f (Block block) = Block $ Set.map f block
+
+calcify :: Block -> Rock -> Rock
+calcify (Block block) (Rock rock) = Rock $ Set.union rock block
 
 lower :: Block -> Block
-lower = Set.map $ second $ subtract 1
+lower = move $ second $ subtract 1
 
 hitsFloor :: Block -> Bool
-hitsFloor block = Set.findMin (Set.map snd block) < 0
+hitsFloor (Block block) = Set.findMin (Set.map snd block) < 0
 
 hitsLeft :: Block -> Bool
-hitsLeft block = Set.findMin (Set.map fst block) < 0
+hitsLeft (Block block) = Set.findMin (Set.map fst block) < 0
 
 hitsRight :: Block -> Bool
-hitsRight block = Set.findMax (Set.map fst block) > 6
+hitsRight (Block block) = Set.findMax (Set.map fst block) > 6
 
 blocks :: [Int -> Block]
-blocks = cycle $ (Set.fromList .) <$>
+blocks = cycle $ fmap (Block . Set.fromList) <$>
   [ \y -> (,y) <$> [2..5]
   , \y -> [(3, y + 2)] <> ((,y + 1) <$> [2..4]) <> [(3, y)]
   , \y -> [(3, y + 2), (3, y + 1)] <> ((,y) <$> [2..4])
